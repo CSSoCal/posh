d274f3ca06d183b4487d74d5bada43aea67b75c0
Lance <lallen@crittentonsocal.org>
2014-07-30 16:34:47 -0700

    Added module

---
+++ CSModules.psm1
@@ -0,0 +1,81 @@
+﻿# Set error colors
+$Host.PrivateData.DebugBackgroundColor = "Black"
+$Host.PrivateData.ErrorBackgroundColor = "Black"
+
+# Start Up
+Get-Content("C:\bin\banner.txt")
+cd $env:USERPROFILE\
+
+# Fancy Pants Custom Prompt
+function prompt
+{
+    Write-Host("[ ") -nonewline -foregroundcolor "Cyan"
+    Write-Host($PWD) -nonewline -foregroundcolor "Cyan"
+    Write-Host(" ]") -foregroundcolor "Cyan"
+    Write-VcsStatus -nonewline
+    Write-Host(">") -nonewline -foregroundcolor "Yellow"
+    return " "
+}
+
+function vpr
+{
+    ise $profile
+}
+
+function say($text)
+{
+    $loadSpeech = New-Object -ComObject SAPI.SpVoice
+    $loadSpeech.Speak($text)
+}
+
+function rdp($site)
+{
+    if($site)
+    {
+        if($site.Length -eq '2')
+        {
+            mstsc /v:cs-$site.ddns.net:54323
+        }
+        else
+        {
+            mstsc /v:$site
+        }
+    }
+    else
+    {
+        $a = Read-Host 'Site code?'
+        mstsc /v:cs-$a.ddns.net:54323
+    }
+}
+
+function deliverFinger()
+{
+[string]$private:Finger = @"
+DQoJICAgICAgICAgLyJcDQoJICAgICAgICB8XC4vfA0KCSAgICAgICAgfCAgIHwNCgkgICAgICAgIHwgICB8DQoJ
+ICAgICAgICB8Pn48fA0KCSAgICAgICAgfCAgIHwNCgkgICAgIC8nXHwgICB8LydcLi4NCgkgL35cfCAgIHwgICB8
+ICAgfCBcDQoJfCAgID1bQF09ICAgfCAgIHwgIFwNCgl8ICAgfCAgIHwgICB8ICAgfCAgIFwNCgl8IH4gICB+ICAg
+fiAgIH4gfGAgICApDQoJfCAgICAgICAgICAgICAgICAgICAvDQoJIFwgICAgICAgICAgICAgICAgIC8NCgkgIFwg
+ICAgICAgICAgICAgICAvDQoJICAgXCAgICBfX19fXyAgICAvDQoJICAgIHwtLS8vJydgXC0tfA0KCSAgICB8ICgo
+ICs9PSkpIHwNCgkgICAgfC0tXF98Xy8vLS18DQo= 
+"@
+	return [System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($Finger)) ;
+}
+
+function psr($pc)
+{
+    Enter-PSSession -ComputerName $pc
+}
+
+function startAD()
+{
+    $session = New-PSSession -ComputerName Chapman
+    Invoke-Command -Session $session {Import-Module ActiveDirectory}
+    Import-PSSession $session -Module ActiveDirectory
+}
+
+function lastloguser($user)
+{
+	Get-ADUser $user -Properties LastLogonDate | Select Name, LastLogonDate
+}
+
+startAD
